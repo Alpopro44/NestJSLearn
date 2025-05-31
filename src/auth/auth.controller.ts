29bf8913c8ad0083a2f3ceb3e340794e0533b498
ExpressJSApp <user@example.com>
2025-05-31 20:55:20 +0300

    Passport konfigürasyonu tamamlandı. Basit Authencation yapısı kuruldu. Guard ayarları yapıldı.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,11 @@
-import {Body, Controller, HttpException, Post} from '@nestjs/common';
+import {Body, Controller, HttpException, Post, Req, UseGuards, Get} from '@nestjs/common';
 import { AuthPayloadDto } from './dto/auth.dto';
 import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
+import { LocalGuard } from './guards/local.guard';
+import { Request } from 'express';
+import { JwtAuthGuard } from './guards/jwt.guard';
+
 
 @Controller('auth')
 export class AuthController {
@@ -10,9 +15,17 @@ export class AuthController {
     }
 
     @Post('login')
+    @UseGuards(LocalGuard)
     login(@Body() authpayload: AuthPayloadDto) {
-        const user = this.authService.validateUser(authpayload);
-        if(!user) throw new HttpException('Invalid Credentials', 401);
+        const user = this.authService.validateUser(authpayload); 
         return user;
+    }
+
+    @Get('status')
+    @UseGuards(JwtAuthGuard)
+    status(@Req() req: Request){
+        console.log("Inside AuthController status Method.");
+        console.log(req.user);
+        return req.user;
     }
 }
